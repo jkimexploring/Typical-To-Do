b63968290e0dd7160ef9503283ce29b8af84ad16
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-08-21 14:53:46 -0500

    add find database function

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var router = express.Router();
+var db = require('../models');
 
 router.get('/', function(req, res){
-    res.send("Hello from ToDos routes");
+    //send all todos from database
+    db.Todo.find().then(function(todos){
+        res.json(todos); //find todos
+    }).catch(function(err){
+        res.send(err); //if there's an error
+    })
 });
 
 module.exports = router;
