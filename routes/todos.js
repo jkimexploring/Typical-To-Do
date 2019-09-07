4ac7aa7a73df373fa89d00e018bb65c297993a3b
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-09-06 20:40:11 -0500

    actually add find

--- routes/todos.js
+++ routes/todos.js
@@ -6,9 +6,9 @@ router.get('/', function(req, res){
    // res.send("Hello from ToDos routes");
    db.Todo.find().then(function(todos){
     res.json(todos); //find todos
-}).catch(function(err){
+    }).catch(function(err){
     res.send(err); //if there's an error
-})
+    })
 });
 
 module.exports = router;
