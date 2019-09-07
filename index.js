49b0555ce3b262ae801746de9ab352f6a1a34cf9
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-09-06 21:32:39 -0500

    lowercase view for consistency

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/Views'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-   res.sendFile(__dirname + '/Views/index.html')
+   res.sendFile(__dirname + '/views/index.html')
 });
 app.use('/api/todos', todoRoutes);
 
