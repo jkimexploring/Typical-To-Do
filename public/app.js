0b7db7565753c09c104e69a8f354db6fdb21a302
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-09-11 06:52:35 -0500

    display addTodos to page

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-$('body').click(function(){alert("CLICKED!!")})
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(addTodos)
+});
+
+function addTodos(todos){
+    //add todos to page
+    todos.forEach(function(todo){
+        var newTodo = $('<li>'+todo.name+'</li>');
+        newTodo.addClass('task');
+        $('.list').append(newTodo);
+    })
+}
