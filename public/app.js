d521fbe8e7e7c20ae547e2efedcd0268105b74d7
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-09-11 07:32:38 -0500

    add functionality- add todo

--- public/app.js
+++ public/app.js
@@ -1,16 +1,38 @@
 $(document).ready(function(){
     $.getJSON("/api/todos")
-    .then(addTodos)
+    .then(addTodos);
+    $('#todoInput').keypress(function(event){
+        if (event.which == 13){
+            createTodo();
+        }
+    })
 });
+
+function addTodo(todo){
+    var newTodo = $('<li>'+todo.name+'</li>');
+    newTodo.addClass('task');
+    if(todo.completed){
+        newTodo.addClass("done")
+    }
+    $('.list').append(newTodo);
+}
 
 function addTodos(todos){
     //add todos to page
     todos.forEach(function(todo){
-        var newTodo = $('<li>'+todo.name+'</li>');
-        newTodo.addClass('task');
-        if(todo.completed){
-            newTodo.addClass("done")
-        }
-        $('.list').append(newTodo);
-    })
+        addTodo(todo);
+    });
 }
+
+function createTodo(){
+    //send request to create new todo
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos',{name: usrInput})
+    .then(function(newTodo){
+      $('#todoInput').val('');
+      addTodos(newTodo);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+  }
